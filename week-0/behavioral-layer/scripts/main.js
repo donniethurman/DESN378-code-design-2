180c76570aae0a759d1903930c39d00755a1b5fe
Donnie Thurman <athurman2@ewu.edu>
2026-01-08 22:08:35 +0000

    Refactor code structure for improved readability and maintainability

--- week-0/behavioral-layer/scripts/main.js
+++ week-0/behavioral-layer/scripts/main.js
@@ -2,3 +2,14 @@
 const myHeading = document.querySelector("h1");
 // Update the text content of the <h1>
 myHeading.textContent = "Hello world!";
+
+const myImage = document.querySelector("img");
+
+myImage.addEventListener("click", () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/image-1.png") {
+    myImage.setAttribute("src", "images/image-2.png");
+  } else {
+    myImage.setAttribute("src", "images/image-1.png");
+  }
+});
