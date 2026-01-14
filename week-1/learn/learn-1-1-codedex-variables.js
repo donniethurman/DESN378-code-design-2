45b789e98ccb9d561829ee107df34143924a9cc3
Donnie Thurman <athurman2@ewu.edu>
2026-01-14 05:44:22 +0000

    Refactor variable declarations and add console logs for weight conversion exercises

--- week-1/learn/learn-1-1-codedex-variables.js
+++ week-1/learn/learn-1-1-codedex-variables.js
@@ -11,29 +11,35 @@
 // - Two let variables 
 // Print them all, then reassign one let variable
 // --------------------------------------------
-​
-​
-​
-​
+​let firstName = "Donnie";
+const favoriteColor = "Yellow";
+
+let currentLocation = "My Room";
+const mood = "Chill";
 // --------------------------------------------
 // EXERCISE 7: DATA TYPES
 // Create variables for your favorite company:
 // 
 // Print them all
 // --------------------------------------------
-​
-​
-​
-​
+​let companyName = "ninth";
+let foundingYear = "2006";
+let isActive = true;
+let fundingAmount;
+
+console.log(companyName);
+console.log(foundingYear);
+console.log(isActive);
+console.log(fundingAmount);
 // --------------------------------------------
 // EXERCISE 8: TEMPERATURE
 // Convert Spokane's temperature from °F to °C
 // Formula: (fahrenheit - 32) / 1.8
 // --------------------------------------------
-​
-​
-​
-​
+​let tempFahrenheit = 42;
+let tempCelsius = (tempFahrenheit - 32)/1.8;
+
+console.log(tempCelsius);
 // --------------------------------------------
 // EXERCISE 9: TIP CALCULATOR
 // Calculate tip and total from a bill
@@ -41,10 +47,11 @@
 // - tipAmount = billAmount * (tipPercent / 100)
 // - total = billAmount + tipAmount
 // --------------------------------------------
-​
-​
-​
-​
+​let weight = 63.5;
+let height = 1.7 ** 2;
+let bmi = weight/height**2;
+
+console.log(bmi);
 // --------------------------------------------
 // EXERCISE 10: PLAYLIST DURATION
 // Calculate total playlist length
@@ -52,6 +59,8 @@
 // - totalMinutes, hours, remainingMinutes
 // Format the output nicely
 // --------------------------------------------
-​
-​
-​
+​let earth_weight = 140;
+let mars_weight = earth_weight * 0.38;
+
+console.log("Your weight on Earth is " + earth_weight + " pounds.");
+console.log("Your weight on Mars is " + mars_weight + " pounds.");
